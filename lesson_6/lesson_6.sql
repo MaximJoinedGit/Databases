909dae741e6ac77d43663a060c6980d25f84de26
MaximJoinedGit <61717547+MaximJoinedGit@users.noreply.github.com>
2020-07-11 12:23:26 +0300

    Finally I've done with all tasks.

--- lesson_6/lesson_6.sql
+++ lesson_6/lesson_6.sql
@@ -1,1 +1,214 @@
-Скоро здесь будет домашнее задание.
+-- Lesson 6.
+USE vk;
+
+-- 2. Создать и заполнить таблицы лайков и постов.
+-- Таблица лайков
+DROP TABLE IF EXISTS likes;
+CREATE TABLE likes (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED NOT NULL,
+  target_id INT UNSIGNED NOT NULL,
+  target_type_id INT UNSIGNED NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+-- Таблица типов лайков
+DROP TABLE IF EXISTS target_types;
+CREATE TABLE target_types (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(255) NOT NULL UNIQUE,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+);
+
+INSERT INTO target_types (name) VALUES 
+  ('messages'),
+  ('users'),
+  ('media'),
+  ('posts');
+
+-- Заполняем лайки
+INSERT INTO likes 
+  SELECT 
+    id, 
+    FLOOR(1 + (RAND() * 300)), 
+    FLOOR(1 + (RAND() * 300)),
+    FLOOR(1 + (RAND() * 4)),
+    CURRENT_TIMESTAMP 
+  FROM messages;
+
+-- Создадим таблицу постов
+CREATE TABLE posts (
+  id INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED NOT NULL,
+  community_id INT UNSIGNED,
+  head VARCHAR(255),
+  body TEXT NOT NULL,
+  media_id INT UNSIGNED,
+  is_public BOOLEAN DEFAULT TRUE,
+  is_archived BOOLEAN DEFAULT FALSE,
+  views_counter INT UNSIGNED DEFAULT 0,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
+-- Таблицу постов заполнили с помощью сайта filldb.
+
+-- 1. Создать все необходимые внешние ключи и диаграмму отношений.
+
+-- profiles
+ALTER TABLE profiles
+  ADD CONSTRAINT profiles_user_id_fk
+    FOREIGN KEY (user_id) REFERENCES users(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT profiles_photo_id_fk
+    FOREIGN KEY (photo_id) REFERENCES media(id)
+      ON DELETE SET NULL;
+
+ALTER TABLE profiles MODIFY COLUMN photo_id INT(10) UNSIGNED;
+
+-- messages
+ALTER TABLE messages
+  ADD CONSTRAINT messages_from_user_id_fk
+    FOREIGN KEY (from_user_id) REFERENCES users(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT messages_to_user_id_fk
+    FOREIGN KEY (to_user_id) REFERENCES users(id)
+      ON DELETE CASCADE;
+
+-- media
+ALTER TABLE media
+  ADD CONSTRAINT media_media_type_id_fk
+    FOREIGN KEY (media_type_id) REFERENCES media_types(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT media_user_id_fk
+    FOREIGN KEY (user_id) REFERENCES users(id)
+      ON DELETE CASCADE;
+
+-- communities_users
+ALTER TABLE communities_users
+  ADD CONSTRAINT communities_users_user_id_fk
+    FOREIGN KEY (user_id) REFERENCES users(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT communities_users_community_id_fk
+    FOREIGN KEY (community_id) REFERENCES communities(id)
+      ON DELETE CASCADE;
+
+-- friendship
+ALTER TABLE friendship
+  ADD CONSTRAINT friendship_status_id_fk
+    FOREIGN KEY (status_id) REFERENCES friendship_statuses(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT friendship_user_id_fk
+    FOREIGN KEY (user_id) REFERENCES users(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT friendship_friend_id_fk
+    FOREIGN KEY (friend_id) REFERENCES users(id)
+      ON DELETE CASCADE;
+
+-- posts
+ALTER TABLE posts
+  ADD CONSTRAINT posts_user_id_fk
+    FOREIGN KEY (user_id) REFERENCES users(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT posts_community_id_fk
+    FOREIGN KEY (community_id) REFERENCES communities(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT posts_media_id_fk
+    FOREIGN KEY (media_id) REFERENCES media(id)
+      ON DELETE SET NULL;
+
+-- likes
+ALTER TABLE likes
+  ADD CONSTRAINT likes_user_id_fk
+    FOREIGN KEY (user_id) REFERENCES users(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT likes_target_type_id_fk
+    FOREIGN KEY (target_type_id) REFERENCES target_types(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT likes_target_users_id_fk
+    FOREIGN KEY (target_id) REFERENCES users(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT likes_target_messages_id_fk
+    FOREIGN KEY (target_id) REFERENCES messages(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT likes_target_media_id_fk
+    FOREIGN KEY (target_id) REFERENCES media(id)
+      ON DELETE CASCADE,
+  ADD CONSTRAINT likes_target_posts_id_fk
+    FOREIGN KEY (target_id) REFERENCES posts(id)
+      ON DELETE CASCADE;
+
+-- 3. Определить кто больше поставил лайков (всего) - мужчины или женщины?
+
+SELECT
+	IF ( (
+	SELECT
+		COUNT(user_id)
+	FROM
+		likes
+	WHERE
+		user_id in (
+		SELECT
+			user_id
+		FROM
+			profiles
+		WHERE
+			gender = 'm') ) > (
+	SELECT
+		COUNT(user_id)
+	FROM
+		likes
+	WHERE
+		user_id in (
+		SELECT
+			user_id
+		FROM
+			profiles
+		WHERE
+			gender = 'w') ), 'men', 'women') AS 'Most active';
+
+-- 4. Подсчитать общее количество лайков десяти самым молодым пользователям (сколько лайков получили 10 самых молодых пользователей).
+
+SELECT birthday FROM profiles WHERE user_id = 60;
+SELECT * FROM profiles ORDER BY birthday DESC LIMIT 10;
+
+SELECT
+	COUNT(*) AS 'Total likes to youngest'
+FROM
+	likes
+WHERE
+	target_type_id = 2
+	AND target_id IN (
+	SELECT
+		user_id
+	FROM
+		(
+		SELECT
+			user_id, birthday
+		FROM
+			profiles
+		ORDER BY
+			birthday DESC
+		LIMIT 10) AS Young);
+
+-- 5. Найти 10 пользователей, которые проявляют наименьшую активность в использовании социальной сети
+-- (критерии активности необходимо определить самостоятельно).
+-- Критерий я определил как отсутствие постов и лайков.
+
+SELECT
+	id,
+	CONCAT(first_name, ' ', last_name) AS 'User'
+FROM
+	users
+WHERE
+	id NOT IN (
+	SELECT
+		user_id
+	FROM
+		posts)
+	AND id NOT IN (
+	SELECT
+		user_id
+	FROM
+		likes)
+LIMIT 10;
