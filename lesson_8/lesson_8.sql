089c04e31885cdef99082d465d734716b4c13677
MaximJoinedGit <61717547+MaximJoinedGit@users.noreply.github.com>
2020-07-19 14:07:24 +0300

    Some fixes to 8th lesson.

--- lesson_8/lesson_8.sql
+++ lesson_8/lesson_8.sql
@@ -6,16 +6,15 @@ USE vk;
 
 SELECT
 	p.gender,
-	COUNT(p.gender) AS 'Итого'
+	COUNT(p.gender) AS `Итого`
 FROM
 	likes l
-JOIN users u
 JOIN profiles p ON
-	u.id = l.user_id
-	AND l.user_id = p.user_id
+	p.user_id = l.user_id
 GROUP BY
 	p.gender
-LIMIT 1;
+ORDER BY `Итого` DESC
+LIMIT 2;
 
 -- 2. Подсчитать общее количество лайков десяти самым молодым пользователям (сколько лайков получили 10 самых молодых пользователей).
 
@@ -41,18 +40,17 @@ FROM
 
 SELECT
 	CONCAT(u.first_name, ' ', u.last_name) AS `User`,
-	(COUNT(DISTINCT l.target_id) + COUNT(DISTINCT p.id) + COUNT(DISTINCT m.to_user_id)) AS `Activity index`
+	(COUNT(DISTINCT m.id) + COUNT(DISTINCT l.id) + COUNT(DISTINCT m2.id)) AS `Activity index`
 FROM
 	users u
-LEFT JOIN likes l ON
-	u.id = l.user_id
-LEFT JOIN posts p ON
-	u.id = p.user_id
 LEFT JOIN messages m ON
 	u.id = m.from_user_id
+LEFT JOIN likes l ON
+	u.id = l.user_id
+LEFT JOIN media m2 ON
+	u.id = m2.user_id
 GROUP BY
 	u.id
 ORDER BY
 	`Activity index`
 LIMIT 10;
-
